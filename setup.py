2da556cd9eeeb378918810cfba9e5d4271881e35
AnDi000111 <02-staende.armlos@icloud.com>
2023-03-06 23:09:04 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+setup(
+    name='openweather',
+    version=VERSION,
+    license='MIT',
+    description='OpenWeather API connector',
+    author='An Di',
+    author_email='bereich_folio0b@icloud.com',
+    url='https://github.com/LeOnAnDi/openwaether',
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.11.2'
+)
