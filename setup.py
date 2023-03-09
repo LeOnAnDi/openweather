82ede7468b877266d9a0c196647d1aa26df79d44
AnDi000111 <02-staende.armlos@icloud.com>
2023-03-09 23:17:04 +0100

    first trunk

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 
 setup(
-    name='openweather',
+    name="openweather",
     version=VERSION,
-    license='MIT',
-    description='OpenWeather API connector',
-    author='An Di',
-    author_email='bereich_folio0b@icloud.com',
-    url='https://github.com/LeOnAnDi/openwaether',
-    packages=find_packages(exclude=('tests', 'docs')),
-    python_requires='>=3.11.2'
+    license="MIT",
+    description="OpenWeather API connector",
+    author="An Di",
+    author_email="bereich_folio0b@icloud.com",
+    url="https://github.com/LeOnAnDi/openwaether",
+    packages=find_packages(exclude=("tests", "docs")),
+    python_requires=">=3.11.2",
 )
